87b84ee6c78dbcbcb1120a7dd23dd70e2f711023
LonelyPenguin <86080520+LonelyPenguin@users.noreply.github.com>
2021-06-28 17:04:24 -0700

    base functionality is working

--- background.js
+++ background.js
@@ -1,13 +1,34 @@
 
 chrome.tabs.onCreated.addListener((tab) => {
     var myTabID = tab.id.toString();
-    var myValue = "Hello";
-    
-    chrome.storage.sync.set({[myTabID]: myValue}, function () {
-        console.log("key: " + myTabID + " value: " +myValue)
+
+    chrome.storage.sync.set({ "latestOpenedTab": myTabID }, function () {
+        console.log("key: " + "latestOpenedTab" + " value: " + myTabID);
     });
 
-    chrome.storage.sync.get(myTabID, result => {
-        console.log(result[myTabID]);
+    chrome.storage.sync.get("latestOpenedTab", result => {
+        console.log(result["latestOpenedTab"]);
     });
 });
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    chrome.storage.sync.get(null, (result) => {
+        console.log(result);
+    });
+
+    let myTabID = tabId.toString()
+    chrome.storage.sync.remove(myTabID)
+
+    chrome.storage.sync.get(null, (result) => {
+        console.log(result);
+    });
+});
+
+//chrome.tabs.onActivated.addListener((activeInfo) => {
+//    let myTabID = activeInfo["tabId"].toString();
+//    chrome.storage.sync.get(myTabID, (result) => {
+//        let tabReason = result[myTabID];
+//        let intTabID = parseInt(myTabID);
+//        chrome.browserAction.setTitle({intTabID, tabReason});
+//    });
+//});
--- popup.js
+++ popup.js
@@ -1,12 +1,27 @@
 // Initialize button with user's preferred color
-//let changeColor = document.getElementById("changeColor");
-//
+
+function handleFormSubmission() {
+
+    let myForm = document.forms["tabReason"];
+    let myInput = myForm["reasonInput"].value;
+
+    chrome.storage.sync.get("latestOpenedTab", (result) => {
+        console.log("get latestOpenedTab in popup.js did work/trigger")
+        let myTabID = result["latestOpenedTab"];
+        chrome.storage.sync.set({[myTabID]: myInput});
+        let intTabID = parseInt(myTabID);
+        chrome.action.setTitle({tabId: intTabID, title: myInput});
+    });
+};
+
+let myForm = document.forms["tabReason"];
+myForm.addEventListener("submit", handleFormSubmission);
 //chrome.storage.sync.get("color", ({ color }) => {
 //    changeColor.style.backgroundColor = color;
 //});
 //
 //// When the button is clicked, inject setPageBackgroundColor into current page
-//changeColor.addEventListener("click", async () => {
+//myForm.onsubmit("click", async () => {
 //    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 //
 //    chrome.scripting.executeScript({
