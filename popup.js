91b8a30c7ed0fd9c97f5d42afe2a229d5b946ed0
LonelyPenguin <86080520+LonelyPenguin@users.noreply.github.com>
2021-06-28 18:09:38 -0700

    display tab reason in popup as well

--- popup.js
+++ popup.js
@@ -2,20 +2,30 @@
 
 function handleFormSubmission() {
 
-    let myForm = document.forms["tabReason"];
+    let myForm = document.forms["setTabReason"];
     let myInput = myForm["reasonInput"].value;
 
     chrome.storage.sync.get("latestOpenedTab", (result) => {
         console.log("get latestOpenedTab in popup.js did work/trigger")
         let myTabID = result["latestOpenedTab"];
-        chrome.storage.sync.set({[myTabID]: myInput});
+        chrome.storage.sync.set({ [myTabID]: myInput });
         let intTabID = parseInt(myTabID);
-        chrome.action.setTitle({tabId: intTabID, title: myInput});
+        chrome.action.setTitle({ tabId: intTabID, title: myInput });
     });
-};
+}
 
-let myForm = document.forms["tabReason"];
+(async function () {
+    let queryOptions = { active: true, currentWindow: true };
+    let tabs = await chrome.tabs.query(queryOptions);
+    let popupTabID = tabs[0].id.toString();
+    chrome.storage.sync.get([popupTabID], (result) => {
+        document.getElementById("currentTabReason").innerHTML = result[popupTabID];
+    });
+})();
+
+let myForm = document.forms["setTabReason"];
 myForm.addEventListener("submit", handleFormSubmission);
+
 //chrome.storage.sync.get("color", ({ color }) => {
 //    changeColor.style.backgroundColor = color;
 //});
